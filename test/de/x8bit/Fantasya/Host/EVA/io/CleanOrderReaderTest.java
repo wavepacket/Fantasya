231816ab0cb48616de99b49edbba1ae52e5d65b8
Ulf Lorenz <ulf82@users.sf.net>
2015-05-05 16:04:13 +0200

    Partial review of CleanOrderReader
    for now, fixed the test a bit

--- test/de/x8bit/Fantasya/Host/EVA/io/CleanOrderReaderTest.java
+++ test/de/x8bit/Fantasya/Host/EVA/io/CleanOrderReaderTest.java
@@ -1,16 +1,56 @@
-package de.x8bit.Fantasya.Host.ManualTests.io;
+package de.x8bit.Fantasya.Host.EVA.io;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
-
-import de.x8bit.Fantasya.Host.EVA.io.CleanOrderReader;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class CleanOrderReaderTest {
 	
-	private static String s00 = "MACHE Holz";
-	private static String s01 = "MACHE 15 Schwert\nBETRETE GEBAEUDE tx\nNACH o w"; // Befehle gehen so durch
-	private static String s02 = "\tHELFE  4 KAEMPFE "; // Leerzeichen rausnehmen
-	private static String s03 = "BESCHREIBE EINHEIT 7 \"bli blopp\\\n bla blupp\""; // Befehle Zusammenfuegen
+	private CleanOrderReader generateReader(String s) {
+		return new CleanOrderReader(new BufferedReader(new StringReader(s)));
+	}
+
+	// note: may be bad style to throw in the constructor (memory leak?), although
+	// this should not be a serious issue for us
+	@Test(expected = IllegalArgumentException.class)
+	public void rejectNullStream() {
+		new CleanOrderReader(null);
+	}
+
+	@Test
+	public void readMultipleCommandsSequentially() {
+		String command1 = "MACHE 15 Schwert";
+		String command2 = "BETRETE GEBAEUDE tx";
+		String command3 = "NACH o w";
+		CleanOrderReader reader = generateReader(command1 + "\n" + command2 + "\n" + command3);
+
+		assertEquals(command1, reader.readOrder());
+		assertEquals(command2, reader.readOrder());
+		assertEquals(command3, reader.readOrder());
+	}
+
+	@Test
+	public void removeWhitespaces() {
+		String trimmedCommand = "HELFE 4 KAEMPFE";
+		String input = "\tHELFE    4 KAEMPFE ";
+		CleanOrderReader reader = generateReader(input);
+
+		assertEquals(trimmedCommand, reader.readOrder());
+	}
+
+	@Test
+	public void joinLines() {
+		String command = "HELFE 4 KAEMPFE";
+		String input = "HELFE 4 \\\nKAEMPFE";
+
+		CleanOrderReader reader = generateReader(input);
+
+		assertEquals(command, reader.readOrder());
+	}
+
+
+	// not yet fixed/migrated remainder
 	private static String s04 = "MACHE Gold\n ; hilfe kommentar\nSTIRB"; // temporäre Kommentare rausnehmen, die am Anfang der Zeile stehen.
 	private static String s05 = "MACHE Gold\n // hilfe nochmal Gold absahnen :)\nSTIRB"; // bleibende Kommentare, die am Anfang der Zeile stehen.
 	private static String s06 = "MACHE Gold // hilfe nochmal Gold absahnen :)\nSTIRB"; // bleibende Kommentare, der nach einem Befehl ohne '"' steht.
@@ -20,30 +60,10 @@ public class CleanOrderReaderTest {
 	private static String s10 = "BESCHREIBE EINHEIT \"bla// hilfe nochmal Gold absahnen :)\"// ... und es macht Spaß\nSTIRB"; // Merkmal des bleibenden Kommentars innerhalb und außerhalb einer Beschreibung
 	private static String s11 = "BESCHREIBE EINHEIT \"bla// hilfe nochmal Gold absahnen :) // tja, mal sehen ;)\"// ... und es macht Spaß\nSTIRB"; // Merkmal des bleibenden Kommentars innerhalb und außerhalb einer Beschreibung
 	private static String s12 = "MACHE 15 Schwert\n\nBETRETE GEBAEUDE tx\n  \nNACH o w"; // Befehle gehen so durch, sind aber leerzeilen bzw. nur mit leerzeichen vorhanden
-	
-	
-	public static void main(String[] args) {
-		// testTrim();
-		testComplete();
-	}
-	
-	/*private static void testTrim() {
-		StringBuffer buf = CleanOrderReader.tempBuf;
-		buf.setLength(0);
-		buf.append(s0);
-		CleanOrderReader.trim(true);
-		System.out.println("'" + buf + "'");
-		buf.setLength(0);
-		buf.append(s2);
-		CleanOrderReader.trim(true);
-		System.out.println("'" + buf + "'");
-	} */
-	
-	private static void testComplete() {
-		CleanOrderReader cOR00 = new CleanOrderReader(new BufferedReader(new StringReader(s00)));
-		CleanOrderReader cOR01 = new CleanOrderReader(new BufferedReader(new StringReader(s01)));
-		CleanOrderReader cOR02 = new CleanOrderReader(new BufferedReader(new StringReader(s02)));
-		CleanOrderReader cOR03 = new CleanOrderReader(new BufferedReader(new StringReader(s03)));
+
+
+	@Test
+	public void testComplete() {
 		CleanOrderReader cOR04 = new CleanOrderReader(new BufferedReader(new StringReader(s04)));
 		CleanOrderReader cOR05 = new CleanOrderReader(new BufferedReader(new StringReader(s05)));
 		CleanOrderReader cOR06 = new CleanOrderReader(new BufferedReader(new StringReader(s06)));
@@ -54,47 +74,11 @@ public class CleanOrderReaderTest {
 		CleanOrderReader cOR11 = new CleanOrderReader(new BufferedReader(new StringReader(s11)));
 		CleanOrderReader cOR12 = new CleanOrderReader(new BufferedReader(new StringReader(s12)));
 		
-		System.out.println("Test 00:");
-		System.out.println("Befehlstext: ");
-		System.out.println(s00 + '\n');
-		System.out.println("Befehle:");
-		String s = cOR00.readOrder();
-		while (s != null) {
-			System.out.println("'" + s + "'");
-			s = cOR00.readOrder();
-		}
-		System.out.println("\nTest 01:");
-		System.out.println("Befehlstext: ");
-		System.out.println(s01 + '\n');
-		System.out.println("Befehle:");
-		s = cOR01.readOrder();
-		while (s != null) {
-			System.out.println("'" + s + "'");
-			s = cOR01.readOrder();
-		}
-		System.out.println("\nTest 02:");
-		System.out.println("Befehlstext: ");
-		System.out.println(s02 + '\n');
-		System.out.println("Befehle:");
-		s = cOR02.readOrder();
-		while (s != null) {
-			System.out.println("'" + s + "'");
-			s = cOR02.readOrder();
-		}
-		System.out.println("\nTest 03:");
-		System.out.println("Befehlstext: ");
-		System.out.println(s03 + '\n');
-		System.out.println("Befehle:");
-		s = cOR03.readOrder();
-		while (s != null) {
-			System.out.println("'" + s + "'");
-			s = cOR03.readOrder();
-		}
 		System.out.println("\nTest 04:");
 		System.out.println("Befehlstext: ");
 		System.out.println(s04 + '\n');
 		System.out.println("Befehle:");
-		s = cOR04.readOrder();
+		String s = cOR04.readOrder();
 		while (s != null) {
 			System.out.println("'" + s + "'");
 			s = cOR04.readOrder();
@@ -172,6 +156,4 @@ public class CleanOrderReaderTest {
 			s = cOR12.readOrder();
 		}
 	}
-	
-	
 }
